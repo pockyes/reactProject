a2a861c973caa400821471b0fcebbba9a0632f97
한아름 <tele1841@xmd.co.kr>
2022-01-24 14:05:55 +0900

    layout 분리 / header, body, footer

--- src/Layouts/Body.js
+++ src/Layouts/Body.js
@@ -1,14 +1,302 @@
 import React from 'react';
-import './index.css';
+import '../index.css';
 
 class Body extends React.Component{
     render(){
         return (
-            <div className="app-body">
-              Body 입니다.
-            </div>
+            <div>
+                <div className="business lato-black-granite-gray-16px">
+                    BUSINESS
+                </div>
+                <div className="text-4 spoqahansansneo-bold-ebony-clay-32px">
+                        상점, 빌딩, 주차장, 창고 …<br />이 세상 모든 유휴공간을 빌려보세요
+                </div>
+                <div className="overlap-group13">
+                        <div className="flex-col">
+                          <div className="overlap-group3">
+                            <div className="b2-c spoqahansansneo-bold-white-18px">
+                              B2C
+                            </div>
+                          </div>
+                          <div className="text-2 poqahansansneo-bold-white-28px">
+                            고객은 가까운 상점 유휴공간에
+                            <br />
+                            물품을 보관하고 <br />
+                            상점주는 추가 수익을 얻습니다
+                          </div>
+                          <div className="group-249">
+                            <div className="text-2-1 spoqahansansneo-medium-white-16px">
+                              자세히 알아보기→
+                            </div>
+                            <img
+                              className="line-15"
+                              src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/line-15@2x.svg"
+                            />
+                          </div>
+                        </div>
+                        <div className="overlap-group18">
+                            <img 
+                                className="mask-group"
+                                src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/mask-group@3x.png"
+                            />
+                          <img
+                            className="group-373"
+                            src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/group-373@3x.png"
+                          />
+                        </div>
+                      </div>
+                      <div className="overlap-group17">
+                        <div className="flex-col-1">
+                          <div className="overlap-group5">
+                            <div className="b2-b spoqahansansneo-bold-white-18px">
+                              B2B
+                            </div>
+                          </div>
+                          <div className="text-2 spoqahansansneo-bold-white-28px">
+                            배송지 근처에
+                            <br />
+                            마이크로 물류 허브를 구축하고
+                            <br />
+                            더욱 빠르게 물건을 배송해보세요
+                          </div>
+                          <div className="group-243">
+                            <div className="text-2-1 spoqahansansneo-medium-white-16px">
+                              제휴/협업 문의→
+                            </div>
+                            <img
+                              className="line-15-1"
+                              src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/line-15@2x.svg"
+                            />
+                          </div>
+                        </div>
+                        <img
+                          className="mask-group-1"
+                          src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/mask-group-1@1x.png"
+                        />
+                      </div>
+                      <div className="overlap-group16">
+                        <div className="flex-col-2">
+                          <div className="overlap-group6">
+                            <div className="b2-b2-c spoqahansansneo-bold-ebony-clay-18px">
+                              B2B2C
+                            </div>
+                          </div>
+                          <div className="text-31">
+                            나의 고객에게 <br />
+                            다양한 배송/픽업 위치를
+                            <br />
+                            제공해보세요
+                          </div>
+                          <div className="group-241">
+                            <div className="text-30">
+                              제휴/협업 문의→
+                            </div>
+                            <img
+                              className="line-15-2"
+                              src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/line-15@2x.svg"
+                            />
+                          </div>
+                        </div>
+                        <img
+                          className="mask-group-2"
+                          src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/mask-group-1@1x.png"
+                        />
+                      </div>
+                      <div className="overlap-group15">
+                        <img
+                          className="rectangle-78"
+                          src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/rectangle-78@1x.svg"
+                        />
+                        <img
+                          className="group-146"
+                          src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/group-146@2x.png"
+                        />
+                        <div className="solution lato-black-granite-gray-16px">
+                          SOLUTION
+                        </div>
+                        <div className="text-1 spoqahansansneo-bold-ebony-clay-32px">
+                          블루웨일팀의 솔루션으로
+                          <br />
+                          전국 1,500개 이상의
+                          <br />
+                          유휴 공간을 연결합니다
+                        </div>
+                        <div className="text-2-2 spoqahansansneo-bold-ebony-clay-32px">
+                          유휴 공간 검색 솔루션
+                          <br />옆 회사에선 이렇게 씁니다
+                        </div>
+                        <div className="history">
+                          HISTORY
+                        </div>
+                        <div className="text-3 spoqahansansneo-bold-white-32px">
+                          다양한 공간을 빠르게 확보하며
+                          <br />
+                          가파르게 성장하고 있습니다
+                        </div>
+                        <div className="group-315">
+                          <div className="group-221">
+                            <div className="text-7 spoqahansansneo-bold-ronchi-48px">
+                              50+
+                            </div>
+                            <div className="text-6 spoqahansansneo-bold-white-18px">
+                              유휴 공간 종류
+                            </div>
+                          </div>
+                          <div className="group-222">
+                            <div className="text-9 spoqahansansneo-bold-ronchi-48px">
+                              1,500+
+                            </div>
+                            <div className="text-8 spoqahansansneo-bold-white-18px">
+                              전국에 확보된 유휴 공간
+                            </div>
+                          </div>
+                          <div className="group-223">
+                            <div className="text-11 spoqahansansneo-bold-ronchi-48px">
+                              10만+
+                            </div>
+                            <div className="text-10 spoqahansansneo-bold-white-18px">
+                              누적 이동 물류량 (2021 예상)
+                            </div>
+                          </div>
+                        </div>
+                        <div className="overlap-group2">
+                          <div className="press lato-black-granite-gray-18px">
+                            PRESS
+                          </div>
+                          <div className="text-21 spoqahansansneo-bold-mine-shaft-32px">
+                            언론에 비친 블루웨일팀
+                          </div>
+                          <div className="group-231">
+                            <div className="flex-row-1">
+                              <div className="gs25 spoqahansansneo-medium-mine-shaft-18px">
+                                GS25, 공유 물품보관 서비스 ‘럭스테이’ 론칭
+                              </div>
+                              <div className="date spoqahansansneo-regular-normal-mountain-mist-16px">
+                                2021.01.25
+                              </div>
+                            </div>
+                            <img
+                              className="line"
+                              src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/line-7@1x.svg"
+                            />
+                            <div className="flex-row">
+                              <div className="text-22 spoqahansansneo-medium-mine-shaft-18px">
+                                [인터뷰] 상점에 짐 맡기는 &#39;특허&#39; 스타트업, 오상혁 럭스테이 대표
+                              </div>
+                              <div className="date-1 spoqahansansneo-regular-normal-mountain-mist-16px">
+                                2021.01.25
+                              </div>
+                            </div>
+                            <img
+                              className="line"
+                              src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/line-7@1x.svg"
+                            />
+                            <div className="flex-row">
+                              <div className="gs25-1 spoqahansansneo-medium-mine-shaft-18px">
+                                “편의점에서 물품 보관해줍니다”···GS25, ‘럭스테이’ 론칭
+                              </div>
+                              <div className="date-2 spoqahansansneo-regular-normal-mountain-mist-16px">
+                                2021.01.25
+                              </div>
+                            </div>
+                            <img
+                              className="line"
+                              src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/line-7@1x.svg"
+                            />
+                            <div className="flex-row">
+                              <div className="text-23 spoqahansansneo-medium-mine-shaft-18px">
+                                상점 여유공간 활용해 부가수익까지, ‘럭스테이’ 호스트 모집
+                              </div>
+                              <div className="date-3 spoqahansansneo-regular-normal-mountain-mist-16px">
+                                2021.01.25
+                              </div>
+                            </div>
+                            <img
+                              className="line"
+                              src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/line-7@1x.svg"
+                            />
+                          </div>
+                        </div>
+                        <div className="group-container">
+                          <div className="text-container border-1px-gray-nurse">
+                            <div className="text-26 spoqahansansneo-bold-tundora-22px">
+                              우리 회사에 딱 맞는 물류 공간
+                              <br />
+                              둘러보고 제안 받기
+                            </div>
+                            <p className="text-27 spoqahansansneo-regular-normal-fuscous-gray-16px">
+                              공간 크기, 위치, 대여료 조건만 알려주세요.
+                              <br />
+                              우리 회사에 딱 맞는 물류 공간을 제안 받을 수<br />
+                              있습니다.
+                            </p>
+                          </div>
+                          <img
+                            className="group-306"
+                            src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/group-306@3x.png"
+                          />
+                        </div>
+                        <div className="overlap-group7 border-1px-gray-nurse">
+                          <img
+                            className="group"
+                            src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/group-307@3x.png"
+                          />
+                          <div className="text-32 spoqahansansneo-bold-tundora-22px">
+                            전기 킥보드 등 개인 모빌리티 <br />
+                            스테이션 만들기
+                          </div>
+                          <p className="text-33 spoqahansansneo-regular-normal-fuscous-gray-16px">
+                            도심 곳곳에 충전, 주차 등을 위한 모빌리티 <br />
+                            스테이션을 설치할 수 있습니다.
+                          </p>
+                        </div>
+                        <img
+                          className="rectangle-114"
+                          src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/rectangle-114@1x.svg"
+                        />
+                        <div className="text-34 spoqahansansneo-bold-white-28px">
+                          비즈니스를 위한 유휴 공간을
+                          <br />
+                          쉽게 찾고 빌려보세요.
+                        </div>
+                        <div className="text-35">
+                          유휴 공간의 가치를 새롭게 정의합니다
+                        </div>
+                        <img
+                          className="group-363"
+                          src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/group-363@3x.png"
+                        />
+                        <div className="overlap-group10 border-1px-gray-nurse">
+                          <img
+                            className="group"
+                            src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/mask-group-2@3x.png"
+                          />
+                          <div className="text-38 spoqahansansneo-bold-tundora-22px">
+                            고객이 원하는 안심 픽업지로
+                            <br />
+                            택배 보내기
+                          </div>
+                          <p className="text-39 spoqahansansneo-regular-normal-fuscous-gray-16px">
+                            10분이면 쇼핑몰에 버튼 설치 완료!
+                            <br />
+                            쇼핑몰 고객은 직접 지정한 안심 픽업지에서
+                            <br />
+                            택배 픽업 서비스를 받을 수 있습니다.
+                          </p>
+                        </div>
+                        <div className="overlap-group11">
+                          <div className="text-20 notosanskr-bold-white-17px">
+                            제휴/협업 문의
+                          </div>
+                        </div>
+                      </div>
+                </div>
+            
+  
         )
     }
 }
+
+
       
 export default Body;
--- src/Layouts/Footer.js
+++ src/Layouts/Footer.js
@@ -1,13 +1,96 @@
 import React from 'react';
-import './index.css';
+import '../index.css';
 
 class Footer extends React.Component{
     render(){
         return (
             <div className="app-footer">
-              Footer 입니다.
+                          <div className="partners lato-black-granite-gray-18px">
+                        PARTNERS
+                      </div>
+                      <div className="text-14 spoqahansansneo-bold-mine-shaft-32px">
+                        믿을 수 있는 파트너와 <br />
+                        함께하는 블루웨일팀
+                      </div>
+                      <img
+                        className="logos-1"
+                        src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/logos-1@3x.png"
+                      />
+                      <div className="overlap-group14">
+                        <Rectangle1 className="rectangle-83" />
+                        <div className="text-16 spoqahansansneo-bold-white-16px">
+                          찾아오시는 길
+                        </div>
+                        <div className="place spoqahansansneo-bold-white-16px">
+                          Contact
+                        </div>
+                        <p className="text-17 spoqahansansneo-regular-normal-white-16px">
+                          서울특별시 종로구 청계천로 85 삼일빌딩 9층
+                        </p>
+                        <div className="supportlugstaycom spoqahansansneo-regular-normal-white-16px">
+                          support@lugstay.com
+                        </div>
+                        <img
+                          className="line-11"
+                          src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/line-11@2x.svg"
+                        />
+                        <img
+                          className="line-12"
+                          src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/line-11@2x.svg"
+                        />
+                        <div className="text-18 spoqahansansneo-bold-white-32px">
+                          블루웨일팀과 더 많은
+                          <br />
+                          이야기를 나눠보세요
+                        </div>
+                        <div className="overlap-group border-1-5px-white">
+                          <div className="text-19 notosanskr-bold-white-17px">
+                            회사소개서 받아보기
+                          </div>
+                        </div>
+                        <div className="overlap-group1">
+                          <div className="text notosanskr-bold-white-17px">
+                            제휴/협업 문의
+                          </div>
+                        </div>
+                        <img
+                          className="group-2"
+                          src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61edfc5d0b6c58ec22ee4462/img/group@1x.png"
+                        />
+                        <img
+                          className="mask-group-3"
+                          src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/mask-group@1x.svg"
+                        />
+                        <Rectangle1 className="rectangle-187" />
+                        <div className="overlap-group9">
+                          <div className="text-36">
+                            공간 등록 문의
+                          </div>
+                        </div>
+                        <div className="text-37">
+                          유휴공간이 있으신가요?
+                          <br />
+                          공간을 제공하고 추가 수익을 얻어보세요
+                        </div>
+                        <img
+                          className="image-17"
+                          src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/image-17@1x.png"
+                        />
+                      </div>
+                      <img
+                        className="image-8"
+                        src="https://cdn.animaapp.com/projects/61e8ac41a0bdd8ea4045e986/releases/61e8ae8d1b80f3096e13ab93/img/image-8@1x.png"
+                      />
             </div>
         )
+    }
+}
+
+class Rectangle1 extends React.Component {
+    render() {
+        const { className } = this.props;
+
+        return <div className={`rectangle-1 ${className || ""}`}></div>;
     }
 }
      
